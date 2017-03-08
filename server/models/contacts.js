e849b26e9d6c36dc1faabe9e8f6f97db32d3e0c9
Joshua Bradford <jjyet@yahoo.com>
2017-03-08 15:15:54 -0500

    initial commit

---
+++ server/models/contacts.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Contacts = sequelize.define('Contacts', {
+    name: DataTypes.STRING,
+    phone_number: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    photo_url: DataTypes.STRING,
+    email: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Contacts;
+};
