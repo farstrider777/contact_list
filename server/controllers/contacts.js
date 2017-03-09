6e6f1ecf90c4870d53fa37e12089e0e42092e3ab
Joshua Bradford <jjyet@yahoo.com>
2017-03-09 06:07:22 -0500

    now listing all the comments works, problems with getindividual contact and update contact

--- server/controllers/contacts.js
+++ server/controllers/contacts.js
@@ -10,9 +10,47 @@ const Contacts = require("../models").Contacts;
         state: req.body.state,
         photo_url: req.body.photo_url,
         email: req.body.email,
-
-    })
+      })
       .then(contacts => res.status(201).send(contacts))
       .catch(error => res.status(400).send(error));
-    }
+    },
+
+    listContacts (req, res) {
+        Contacts.findAll({
+          // where: {
+          //   id: req.params.id
+          // }
+        })
+        .then(contacts => res.status(201).send(contacts))
+        .catch(error => res.status(400).send(error));
+      },
+
+    listOneContact (req, res) {
+        Contacts.findAll({
+          where: {
+            id: req.params.id
+          }
+        })
+        .then(contacts => res.status(201).send(contacts))
+        .catch(error => res.status(400).send(error));
+      },
+
+      // updateContact (req, res) {
+      //     Contacts.update({
+      //
+      //       updatedAt: null,
+      //       }, {
+      //       where: {
+      //         deletedAt: {
+      //           $ne: null
+      //         }
+      //       }
+      //
+      //     })
+      //     .then(contacts => res.status(201).send(contacts))
+      //     .catch(error => res.status(400).send(error));
+      //   },
+
+
+
   };
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,4 +3,7 @@ const ContactsController = require("../controllers/contacts");
 
 module.exports = (app) => {
   app.post('/contacts', ContactsController.create);
+  app.get('/contacts', ContactsController.listContacts);
+  app.get('/contacts', ContactsController.listOneContact);
+  //app.update('/contacts', ContactsController.updateContact);
 };
